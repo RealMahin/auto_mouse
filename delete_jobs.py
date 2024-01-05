bf6c7c840eee9ed222c6f6c04f8b3865e39c12d0
Steven M <93171024+RealMahin@users.noreply.github.com>
2024-01-05 00:43:04 -0500

    for brett

---
+++ delete_jobs.py
@@ -0,0 +1,28 @@
+import pyautogui
+
+# DO NOT CHANGE FAILSAFE!! When the cursor stops moving.
+# Move your mouse to the upper left corner of the screen to abort.
+pyautogui.FAILSAFE = True
+pyautogui.PAUSE = 0.4
+
+# minimum is 0.1
+cursor_speed = 0.1
+
+delete_this_many = 5
+
+# Game Time
+for i in range(delete_this_many):
+    # 1: clicks the "Actions" button
+    pyautogui.moveTo(1799, 544, cursor_speed)
+    pyautogui.click()
+    print("click Actions")
+
+    # 2: moves to and clicks the "Close Job" button
+    pyautogui.moveTo(1745, 602, cursor_speed)
+    pyautogui.click()
+    print("click Close Job")
+
+    # 3: moves to and clicks the "Yes, close it!" button
+    pyautogui.moveTo(842, 670, cursor_speed)
+    pyautogui.click()
+    print("click Yes, close it")
