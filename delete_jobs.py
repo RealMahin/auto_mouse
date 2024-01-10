58bc2649bcde470d823403f409c90012f951724e
Steven M <93171024+RealMahin@users.noreply.github.com>
2024-01-09 22:17:16 -0500

    add maximum for task loop

--- delete_jobs.py
+++ delete_jobs.py
@@ -1,7 +1,7 @@
 import pyautogui
 from sys import argv
 
-# DO NOT CHANGE FAILSAFE!! When the cursor stops moving.
+# DO NOT CHANGE FAILSAFE!! When the cursor stops moving...
 # Move your mouse to the upper left corner of the screen to abort.
 pyautogui.FAILSAFE = True
 pyautogui.PAUSE = 0.3
@@ -9,22 +9,27 @@ pyautogui.PAUSE = 0.3
 script, task_counter = argv
 task_number = int(task_counter)
 
+
 # minimum is 0.1
 cursor_speed = 0.2
 
 # Game Time
 for i in range(task_number):
-    # 1: clicks the "Actions" button
-    pyautogui.moveTo(1799, 544, cursor_speed)
-    pyautogui.click()
-    print("click Actions")
+    if (task_number <= 100):
+        # 1: clicks the "Actions" button
+        pyautogui.moveTo(1799, 544, cursor_speed)
+        pyautogui.click()
+        print("click Actions")
 
-    # 2: moves to and clicks the "Close Job" button
-    pyautogui.moveTo(1745, 602, cursor_speed)
-    pyautogui.click()
-    print("click Close Job")
+        # 2: moves to and clicks the "Close Job" button
+        pyautogui.moveTo(1745, 602, cursor_speed)
+        pyautogui.click()
+        print("click Close Job")
 
-    # 3: moves to and clicks the "Yes, close it!" button
-    pyautogui.moveTo(842, 670, cursor_speed)
-    pyautogui.click()
-    print("click Yes, close it")
+        # 3: moves to and clicks the "Yes, close it!" button
+        pyautogui.moveTo(842, 670, cursor_speed)
+        pyautogui.click()
+        print("click Yes, close it")
+    else:
+        print("The maximum is 100")
+        break
