92c932353aacca654578cff03e8ed289b6ab53ac
RealMahin <damahin@gmail.com>
2024-01-17 10:57:26 -0500

    change step variable names to position

--- auto_mouse.py
+++ auto_mouse.py
@@ -8,9 +8,9 @@ pyautogui.FAILSAFE = True
 # Configure
 script, task_counter = argv # determines what arguments are typed on the command line
 task_number = int(task_counter) # converts tasks_counter from a string to an integer
-step1_x, step1_y = 1799, 544
-step2_x, step2_y = 1745, 602
-step3_x, step3_y = 842, 670
+position1_x, position1_y = 1799, 544
+position2_x, position2_y = 1745, 602
+position3_x, position3_y = 842, 670
 
 # Options
 pyautogui.PAUSE = 0.6 # how long the cursor stops at each coordinate
@@ -22,17 +22,17 @@ maximum_tasks = 100 # 100 is recommended
 for i in range(task_number):
     if (task_number <= maximum_tasks):
         # 1: moves to 1st position and clicks
-        pyautogui.moveTo(step1_x, step1_y, cursor_speed)
+        pyautogui.moveTo(position1_x, position1_y, cursor_speed)
         pyautogui.click()
         print("1st Position")
 
         # 2: moves to 2nd position and clicks
-        pyautogui.moveTo(step2_x, step2_y, cursor_speed)
+        pyautogui.moveTo(position2_x, position2_y, cursor_speed)
         pyautogui.click()
         print("2nd Position")
 
         # 3: moves to 3rd position and clicks
-        pyautogui.moveTo(step3_x, step3_y, cursor_speed)
+        pyautogui.moveTo(position3_x, position3_y, cursor_speed)
         pyautogui.click()
         print("3rd Position")
     else:
